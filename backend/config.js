3d94d301d470d4c6b1d82faf3aea0266f8a870e1
Gavindi Liyanwala <kaviliyanwala@gmail.com>
2025-03-21 18:40:16 +0530

    update with mongo Atlas

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT=5555;
 
 export const mongoDBURL=
-'mongodb+srv://kaviliyanwala:<mongoroot>@book-shop-mern.igfwu.mongodb.net/?retryWrites=true&w=majority&appName=Book-shop-mern';
+'mongodb+srv://kaviliyanwala:<mongoroot>@bookshop.v2zeb.mongodb.net/?retryWrites=true&w=majority&appName=bookshop';
