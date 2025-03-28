dcef3e93fea24ed46f904fe768875d44b0d93a18
Gavindi Liyanwala <kaviliyanwala@gmail.com>
2025-03-28 23:52:27 +0530

    save new book

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,42 @@ import { Book } from './models/bookModels.js';
 
 const app =express();
 
+app.use(express.json());
+
 app.get('/',(request,response)=>{
     console.log(request)
     return response.status(234).send('Welcome');
 });
 
-app.post('/books')
+app.post('/books',async(request,respone)=>
+{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear 
+        )
+        {
+            return response.status(400).send({message:'send all required field title,author,publishYear'});
+        }
+        const newBook={
+            title:request.body.title,
+            author:request.body.author ,
+            publishYear:request.body.publishYear ,
+        };
 
+        const book= await Book.create(newBook);
+
+        return response.status(201).send(book);
+
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+app.get();
 
 mongoose.connect(mongoDBURL)
 .then(()=>{
